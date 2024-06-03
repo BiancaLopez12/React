ae8fe733f4f64c0a31fb126a61c93d8e743fff2c
biancajoana <biancajoanalopez@gmail.com>
2024-06-03 16:55:23 -0300

    3deJunio/React

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,13 @@
+import { MiPrimerComponente } from "./components/miPrimerComponente/MiPrimerComponente";
+const MyFuction = () => {
+  <>Hola</>;
+};
+function App() {
+  return (
+    <>
+      <MyFuction />
+      <MiPrimerComponente name={"Bianca"} last_name={"Lopez"} />
+    </>
+  );
+}
+export default App;
---
+++ vite-project/src/components/miPrimerComponente/MiPrimerComponente.jsx
@@ -0,0 +1,7 @@
+export function MiPrimerComponente({ name, last_name }) {
+  return;
+  <>
+    Mi nombre es Bianca {name} {last_name}
+    <img src="./imgs/foto.jpg" alt="" />
+  </>;
+}
